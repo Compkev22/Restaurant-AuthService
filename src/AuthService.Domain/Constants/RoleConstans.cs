b352b90fc44753421f472d23ef895a13f23d65b8
Informatica <informatica@alumno.local>
2026-05-05 12:00:34 -0600

    Fix: Correcion de roles

--- src/AuthService.Domain/Constants/RoleConstans.cs
+++ src/AuthService.Domain/Constants/RoleConstans.cs
@@ -2,10 +2,10 @@ namespace AuthService.Domain.Constants;
 
 public class RoleConstants
 {
-    public const string PLATFORM_ADMIN_ROLE = "PLATFORM_ADMIN_ROLE";
-    public const string BRANCH_ADMIN_ROLE = "BRANCH_ADMIN_ROLE";
-    public const string EMPLOYEE_ROLE = "EMPLOYEE_ROLE";
-    public const string CLIENT_ROLE = "CLIENT_ROLE";
+    public const string PLATFORM_ADMIN_ROLE = "PLATFORM_ADMIN";
+    public const string BRANCH_ADMIN_ROLE = "BRANCH_ADMIN";
+    public const string EMPLOYEE_ROLE = "EMPLOYEE";
+    public const string CLIENT_ROLE = "CLIENT";
 
     public static readonly string[] AllowedRoles = 
     [
